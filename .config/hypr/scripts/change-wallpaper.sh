138de4fb356258c698b1e710a1d1f15f19319700
tox <tox@tox.wtf>
2026-06-04 04:25:17 -0500

    feat: add `change-wallpaper.sh`

---
+++ .config/hypr/scripts/change-wallpaper.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+wallpaper_directory="$HOME/pics/wp"
+tempfile=$(mktemp)
+
+fd -tf --base-directory "$wallpaper_directory" | shuf | fzm > $tempfile
+
+[[ -s $tempfile ]] && awww img "$wallpaper_directory/$(<"$tempfile")" -t any --transition-fps 240 --transition-duration 1
+rm -f $tempfile
